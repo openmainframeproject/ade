2140be1238b1946c8619ec35b5c1514f40d35ed3
Jim Caffrey <caffreyj@us.ibm.com>
2016-09-02 15:29:28 -0400

    remove most actions from shell script

--- runSonarQubeAnalysis.sh
+++ runSonarQubeAnalysis.sh
@@ -6,22 +6,22 @@
 # - SONAR_HOST_URL => should point to the public URL of the SQ server (e.g. for Nemo: https://nemo.sonarqube.org)
 # - SONAR_TOKEN    => token of a user who has the "Execute Analysis" permission on the SQ server
 
-installSonarQubeScanner() {
-	export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-2.6
-	rm -rf $SONAR_SCANNER_HOME
-	mkdir -p $SONAR_SCANNER_HOME
-	curl -sSLo $HOME/.sonar/sonar-scanner.zip http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip
-	unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-	rm $HOME/.sonar/sonar-scanner.zip
-	export PATH=$SONAR_SCANNER_HOME/bin:$PATH
-}
+#installSonarQubeScanner() {
+#	export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-2.6
+#	rm -rf $SONAR_SCANNER_HOME
+#	mkdir -p $SONAR_SCANNER_HOME
+#	curl -sSLo $HOME/.sonar/sonar-scanner.zip http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip
+#	unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+#	rm $HOME/.sonar/sonar-scanner.zip
+#	export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+#}
 
 # Install the SonarQube Scanner
 # TODO: Would be nice to have it pre-installed by Travis somehow
 #installSonarQubeScanner
 # And set the related JVM options - this is where the size of the JVM can be increased if required (e.g. "-Xmx1G -Xms128m").
-export SONAR_SCANNER_OPTS="-server"
-echo $USER
+#export SONAR_SCANNER_OPTS="-server"
+#echo $USER
 # And run the analysis
 # It assumes that there's a sonar-project.properties file at the root of the repo
 #if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
